c36328b2599c524decb6c23d262109257f4072e6
natsu19 <crookedjoker000@gmail.com>
2022-07-29 10:12:22 +0800

    Update 2022-07-29T02:11:11.611Z

---
+++ assets/js/all.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var navToggle = document.querySelector(".navToggle");
+var nav = document.querySelector(".nav"); //點漢堡icon時
+
+navToggle.onclick = function () {
+  //增加一個active的class
+  nav.classList.toggle("active");
+};
+//# sourceMappingURL=all.js.map
