cd20e8a75a4d3a795a05ae4ea292624234745b88
natsu19 <crookedjoker000@gmail.com>
2022-08-11 15:55:40 +0800

    全站初次完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,9 +1,8 @@
-let navToggle = document.querySelector(".navToggle")
-  let nav = document.querySelector(".nav")
+  $(document).ready(function () {
+    $("#head").load("_head.html");
+    $("#header").load("_header.html");
+    $("#footer").load("_footer.html");
+    $("#page").load("_page.html");
+  });
 
-  
-  //點漢堡icon時
-  navToggle.onclick = function(){
-    //增加一個active的class
-    nav.classList.toggle("active");
-  }
+
